7703e86f525dcd338d4e374a5ec09359183494fe
GitHub Action <action@github.com>
2021-03-15 14:51:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/jdacruz/chhackhowmuchperpod/MyBundle.kt
+++ src/main/kotlin/com/github/jdacruz/chhackhowmuchperpod/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jdacruz.chhackhowmuchperpod
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jdacruz/chhackhowmuchperpod/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jdacruz.chhackhowmuchperpod.services
+
+import com.github.jdacruz.chhackhowmuchperpod.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
